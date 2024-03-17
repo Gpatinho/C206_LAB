9ddb58fcb6563cdcab05e66ba04b1a9efe253e18
Gpatinho <160787174+Gpatinho@users.noreply.github.com>
2024-03-17 16:53:42 -0300

    Update PrimeiraAula_Sem_quebrade_Linha.java

--- PrimeiraAula_Sem_quebrade_Linha.java
+++ PrimeiraAula_Sem_quebrade_Linha.java
@@ -1,6 +1,6 @@
 public class PrimeiraAula_Sem_quebrade_Linha {
     public static void main(String[] args) {
-        System.out.println(" Minha primeira aula de Poo com java");
+        System.out.print(" Minha primeira aula de Poo com java");
 
     }
 }
